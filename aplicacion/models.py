9e322a181990395a5ee425f35dcc0c2410e84027
Manuel <poofcefnunsj@unsj-cuim.edu.ar>
2020-06-25 17:52:11 -0300

    modificacion models.py

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,4 +1,4 @@
-from __main__ import app
+from aplicacion import app
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
@@ -7,13 +7,11 @@ class Usuario(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	nombre = db.Column(db.String(80), nullable=False)
 	correo = db.Column(db.String(120), unique=True, nullable=False)
-	clave = db.Column(db.String(120), nullable=False)    
+	clave = db.Column(db.String(120), nullable=False)
 	comentario = db.relationship('Comentario', backref='usuario', cascade="all, delete-orphan", lazy='dynamic')
-	
+
 class Comentario(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	fecha = db.Column(db.DateTime)
 	contenido = db.Column(db.Text)
-	usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))    
-    
-    
+	usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
