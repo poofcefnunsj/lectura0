3eddca4d61d8b687b2019433e5265ed3667e41f8
FCEFN-UNSJ <63244005+poofcefnunsj@users.noreply.github.com>
2020-07-01 17:56:08 -0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,5 @@
 import click
 @click.command(name='saludos')
-@with_appcontext
 @click.option("--count", default=1, help="Number of greetings.")
 @click.option("--name", prompt="Your name", help="The person to greet.")
 def hello(count, name):
